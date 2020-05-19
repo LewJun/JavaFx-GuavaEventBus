d6c6f28248fb248f65e4ccbbebe712e2791cabd2
LewJun <lewjun072@sina.com>
2020-05-19 23:45:08 +0800

    TextInputDialog的使用

--- src/main/java/org/example/lewjun/modules/dialog/DialogController.java
+++ src/main/java/org/example/lewjun/modules/dialog/DialogController.java
@@ -2,8 +2,10 @@ package org.example.lewjun.modules.dialog;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.TextInputDialog;
 import org.example.lewjun.base.BaseController;
 
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -16,9 +18,24 @@ public class DialogController extends BaseController {
         dialog.setHeaderText("Choose your favourite number.");
         dialog.setContentText("Your favourite number:");
         final Optional<String> choice = dialog.showAndWait();
-        final String ret = choice.map(it -> {
-            return "Other".equals(it) ? "Unfortunately your favourite number is not available"
-                    : String.format("Nice! I like %s", it);
+        final String ret = choice.map(it -> "Other".equals(it) ? "Unfortunately your favourite number is not available"
+                : String.format("Nice! I like %s", it)).orElse("click the X or cancel");
+        System.out.println(ret);
+    }
+
+    public void openTextInputDialog(final ActionEvent event) {
+        final TextInputDialog dialog = new TextInputDialog("0702");
+        dialog.setTitle("Favourite number?");
+        dialog.setHeaderText("Choose your favourite number.");
+        dialog.setContentText("Your favourite number:");
+
+        final Optional<String> result = dialog.showAndWait();
+        final String ret = result.map(it -> {
+            try {
+                return MessageFormat.format("Nice! I like {0}", Integer.parseInt(it));
+            } catch (final Exception e) {
+                return MessageFormat.format("Unfortunately {0} is not int", it);
+            }
         }).orElse("click the X or cancel");
         System.out.println(ret);
     }

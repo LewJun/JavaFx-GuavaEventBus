a43c2a2f081c6bb913895c4e664aeb7f35c48281
LewJun <lewjun072@sina.com>
2020-05-19 23:29:32 +0800

    默认实现initialize方法

--- src/main/java/org/example/lewjun/base/BaseController.java
+++ src/main/java/org/example/lewjun/base/BaseController.java
@@ -3,8 +3,16 @@ package org.example.lewjun.base;
 import javafx.fxml.Initializable;
 import org.example.lewjun.util.EventBusUtil;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public abstract class BaseController implements Initializable {
     public void postEvent(final BaseEvent event) {
         EventBusUtil.post(event);
+    }
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+
     }
 }
